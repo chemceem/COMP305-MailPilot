464a4abc47a24030de5f6000befc10cc7ecf3878
chemceem <chemceem@gmail.com>
2016-01-26 11:55:39 -0500

    added objects, attached keyboard input

--- Assets/_Scripts/OceanController.cs
+++ Assets/_Scripts/OceanController.cs
@@ -5,30 +5,30 @@ public class OceanController : MonoBehaviour {
 
 	//PRIVATE INSTANCE VARIABLE
 	private Transform _transform;
-    private Vector2 _currentPosition;
+	private Vector2 _currentPosition;
 
-    //PUBLIC INSTANCE VARIABLES
-    public float speed = 5;
+	//PUBLIC INSTANCE VARIABLES
+	public float speed = 5;
 
 	// Use this for initialization
 	void Start () {
-        //make a reference with the transform component
+		//make a reference with the transform component
 		this._transform = gameObject.GetComponent<Transform>();
 
-        //reset the ocean sprite to the top
-        this.Reset();
+		//reset the ocean sprite to the top
+		this.Reset();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this._currentPosition = this._transform.position;
-        this._currentPosition -= new Vector2(0, speed);
-        this._transform.position = this._currentPosition;
+		this._currentPosition = this._transform.position;
+		this._currentPosition -= new Vector2(0, speed);
+		this._transform.position = this._currentPosition;
 
-        if (this._currentPosition.y <= -480) { Reset();}
+		if (this._currentPosition.y <= -480) { Reset();}
 	}
 
-    void Reset() {
-        this._transform.position = new Vector2(0, 480f);
-    }
+	void Reset() {
+		this._transform.position = new Vector2(0, 480f);
+	}
 }
---
+++ Assets/_Scripts/PlaneController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+
+    //PRIVATE INSTANCE VARIABLES
+    private float _playerInput;
+    private Transform _transform;
+    private Vector2 _currentPosition;
+
+    //PUBLIC INSTANCE VARIABLES
+    public float speed = 5f;
+
+    // Use this for initialization
+    void Start () {
+        this._transform = gameObject.GetComponent<Transform>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        this._currentPosition = this._transform.position;
+        this._playerInput = Input.GetAxis("Horizontal");
+
+        if (this._playerInput > 0)
+        {
+            this._currentPosition += new Vector2(this.speed, 0);
+        }
+
+        if (this._playerInput < 0)
+        {
+            this._currentPosition -= new Vector2(this.speed, 0);
+        }
+
+        this._transform.position = this._currentPosition;
+        Debug.Log(_playerInput);
+    }
+
+}
