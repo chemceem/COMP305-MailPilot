fcdafd50754621f89bcfa30dd73acb1c9a8a00c6
chemceem <chemceem@gmail.com>
2016-01-28 22:46:03 -0500

    switching line endings LF/CLRF

---
+++ Assets/_Scripts/GameContoller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameContoller : MonoBehaviour {
+
+	//PUBLIC INSTANCE VARIABLES
+	public int cloudNumber = 3;    
+    public CloudController cloud;
+	
+	// Use this for initialization
+	void Start () {
+        this._initialize();        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+    }	
+
+	//***** Private methods *****
+	private void _initialize()	{
+        for (int cloudCount = 0; cloudCount < this.cloudNumber; cloudCount++)
+        {			
+            Instantiate(cloud.gameObject);
+        }
+	}
+}
